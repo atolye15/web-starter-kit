0d3c1d382cef5b73d42b47a4934c4ca1bc423f11
Hasan Aydoğdu <hasan_aydogdu@windowslive.com>
2014-09-14 16:38:38 +0300

    Düzenlemeler yapıldı.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,54 +8,113 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
-
         banner: '/*!\n' +
-                ' * pkg.name\n' +
+                ' * <%= pkg.description %>\n' +
+                ' * <%= pkg.author.name %> < <%= pkg.author.email %> >\n' +
                 ' * Version <%= pkg.version %> ( <%= grunt.template.today("dd-mm-yyyy") %> )\n'+
                 ' */\n',
-        sync: {
-            all: {
+        watch: {
+            css: {
                 options: {
-                    sync: ['version'],
-                    from: 'package.json',
-                    to: 'bower.json'
+                    livereload: true
+                },
+                files: 'src/sass/*.scss',
+                tasks: 'sass'
+            },
+            html: {
+                options: {
+                    livereload: true
+                },
+                files: 'src/*.html',
+                tasks: 'template:development'
+            }
+        },
+        template: {
+
+            development: {
+                options: {
+                    data: {
+                        'cssDir': 'src/css',
+                        'jsDir': 'src/js',
+                        'cssFileName': 'style',
+                        'jsFileName': 'main'
+                    }
+                },
+                files: {
+                    'index.html': ['src/index.html']
+                }
+            },
+            live: {
+                options: {
+                    data: {
+                        'cssDir': 'css',
+                        'jsDir': 'js',
+                        'cssFileName': 'style.min',
+                        'jsFileName': 'main.min'
+                    }
+                },
+                files: {
+                    'index.html': ['src/index.html']
                 }
             }
         },
         sass: {
             dist: {
                 files: {
-                    'css/style.css': 'src/sass/style.scss'
+                    'src/css/style.css': 'src/sass/style.scss'
                 }
             }
         },
-        watch: {
-            files: 'src/*.scss',
-            tasks: 'default'
-        },
         autoprefixer: {
-           options: {
-               browsers: [
-                   'Android 2.3',
-                   'Android >= 4',
-                   'Chrome >= 20',
-                   'Firefox >= 24', // Firefox 24 is the latest ESR
-                   'Explorer >= 8',
-                   'iOS >= 6',
-                   'Opera >= 12',
-                   'Safari >= 6'
-               ]
+            options: {
+                browsers: [
+                    'Android 2.3',
+                    'Android >= 4',
+                    'Chrome >= 20',
+                    'Firefox >= 24', // Firefox 24 is the latest ESR
+                    'Explorer >= 9',
+                    'iOS >= 6',
+                    'Opera >= 12',
+                    'Safari >= 6'
+                ]
             },
-            src: 'css/main.css',
-            dest: 'css/main.css'
+            prefix : {
+                src: 'src/css/style.css',
+                dest: 'src/css/style.css'
+            }
+        },
+        cssmin: {
+            build: {
+                options: {
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    'src/css/style.min.css': 'src/css/style.css'
+                }
+            }
         },
         jshint: {
             // define the files to lint
-            files: ['gruntfile.js', 'src/**/*.js'],
+            files: ['gruntfile.js', 'src/js/*.js'],
             // configure JSHint (documented at http://www.jshint.com/docs/)
             options: {
-                jshintrc: 'js/.jshintrc'
+                jshintrc: '.jshintrc'
             }
+        },
+        uglify: {
+            build: {
+                options: {
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    'js/main.min.js': 'src/js/main.js'
+                }
+            }
+        },
+        clean:{
+            css: ['css'],
+            js: ['js'],
+            img: ['img']
         },
         concat: {
             css: {
@@ -75,37 +134,69 @@ module.exports = function(grunt) {
                 dest: 'js/main.js'
             }
         },
-        uglify: {
-            build: {
+        sync: {
+            all: {
                 options: {
-                    banner: '<%= banner %>'
-                },
-                files: {
-                    'js/main.min.js': 'js/main.js'
+                    sync: ['version'],
+                    from: 'package.json',
+                    to: 'bower.json'
                 }
             }
         },
-        cssmin: {
-            build: {
+        imagemin: {
+            dynamic: { // Another target
                 options: {
-                    banner: '<%= banner %>'
+                    optimizationLevel: 7
                 },
-                files: {
-                    'style.css': 'style.min.css'
-                }
+                files: [{
+                    expand: true, // Enable dynamic expansion
+                    cwd: 'src/', // Src matches are relative to this path
+                    src: ['img/**/*.{png,jpg,gif}'], // Actual patterns to match
+                    dest: '' // Destination path prefix
+                }]
+            }
+        },
+        copy: {
+            js: {
+                expand: true,
+                cwd: 'src',
+                src: ['js/**/*'],
+                dest: '',
+                filter: 'isFile'
+            },
+            css: {
+                expand: true,
+                cwd: 'src',
+                src: ['css/**/*.css', '!css/sprites.css', '!css/fonts/**'],
+                dest: '',
+                filter: 'isFile'
+            },
+            fonts: {
+                expand: true,
+                cwd: 'src',
+                src: ['css/fonts/**'],
+                dest: '',
             }
         }
     });
 
-    grunt.registerTask('default', 'sass');
-    grunt.registerTask('deploy', ['compass','uglify']);
+    grunt.registerTask( 'compressimg', [ 'clean:img', 'imagemin' ] );
+
+    grunt.registerTask('deploy', ['clean:js', 'clean:css', 'template:live', 'sass', 'autoprefixer', 'cssmin', 'jshint', 'uglify', 'sync', 'copy', 'concat']);
+
+    grunt.registerTask( 'build', ['clean:js', 'clean:css', 'sass', 'autoprefixer', 'cssmin', 'jshint', 'uglify', 'copy', 'concat'] );
+
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-template');
     grunt.loadNpmTasks('grunt-npm2bower-sync');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 };
