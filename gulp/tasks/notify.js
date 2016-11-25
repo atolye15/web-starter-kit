74223861a3dc31b75ab7ae727ec96f6a5b33bbf0
Hasan Aydoğdu <hsnaydd@gmail.com>
2016-11-25 16:19:43 +0300

    notify taskında console loging disable edildi

--- gulp/tasks/notify.js
+++ gulp/tasks/notify.js
@@ -1,6 +1,9 @@
 
 module.exports = function({gulp, configs, $}, text) {
   return function() {
+    // Disable console logging
+    $.notify.logLevel(1);
+
     return gulp.src('')
       .pipe($.notify(text));
   };
