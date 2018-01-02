3941fe559f8faa40dc490c06f435ed375041b9ee
Hasan Aydoğdu <hsnaydd@gmail.com>
2018-01-02 16:13:03 +0300

    Upgraded packages and changes done exlained in details - integrated stylelint instead of scss-lint - prefered airbnb -base eslint instead of google - Added prettier - Added vscode workspace settings

--- gulp/tasks/styles/lint.js
+++ gulp/tasks/styles/lint.js
@@ -1,11 +1,12 @@
-
-module.exports = function({gulp, configs, $, browserSync}) {
+module.exports = function({ gulp, configs, $ }) {
   return function() {
-    return gulp.src([
-      `${configs.paths.src}/sass/**/*.scss`, `!${configs.paths.src}/sass/vendors/**`
-    ])
-      .pipe($.plumber({errorHandler: $.notify.onError('Hata: <%= error.message %>')}))
-      .pipe($.scssLint())
-      .pipe(browserSync.active ? $.util.noop() : $.scssLint.failReporter('E'));
+    return gulp
+      .src([`${configs.paths.src}/sass/**/*.scss`, `!${configs.paths.src}/sass/vendors/**`])
+      .pipe($.plumber())
+      .pipe(
+        $.stylelint({
+          reporters: [{ formatter: 'string', console: true }],
+        }),
+      );
   };
 };
