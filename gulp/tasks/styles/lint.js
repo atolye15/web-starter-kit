276b9664087977bcbcb9b7868ae007f7da129e4e
Hasan Aydoğdu <hsnaydd@gmail.com>
2018-01-02 17:11:32 +0300

    eslint prettier conflicts fixed

--- gulp/tasks/styles/lint.js
+++ gulp/tasks/styles/lint.js
@@ -1,10 +1,11 @@
-module.exports = function({ gulp, configs, $ }) {
+module.exports = function({ gulp, configs, $, browserSync }) {
   return function() {
     return gulp
       .src([`${configs.paths.src}/sass/**/*.scss`, `!${configs.paths.src}/sass/vendors/**`])
       .pipe($.plumber())
       .pipe(
         $.stylelint({
+          failAfterError: !browserSync.active,
           reporters: [{ formatter: 'string', console: true }],
         }),
       );
