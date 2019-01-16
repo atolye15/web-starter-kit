1d0e9f176f1dfb85ca59fcf99d37e9c1f13b5c04
Tugay Baltacı <tugaybaltaci@gmail.com>
2019-01-16 14:17:09 +0300

    create-component updated

--- cli/create-component.js
+++ cli/create-component.js
@@ -1,57 +1,74 @@
 #!/usr/bin/env node
 
+/**
+ * Atolye15 Web Starter Kit Command Line Tools
+ *
+ * Usage:
+ *  yarn create-component component-name
+ *
+ * Description:
+ *  Creates a component includes template, styleguide-template and scss file.
+ *  CSS Class: c-component-name
+ *  SCSS Name: _c-component-name.scss
+ *  Template Name: c-component-name.twig
+ *  Styleguide Template Name: c-component-name-styleguide.twig
+ *  Styleguide Section Name: ComponentName
+ */
+
 /* eslint-disable import/no-extraneous-dependencies */
 
 const fs = require('fs');
 const shell = require('shelljs');
 
-const componentNameCapital = name => {
+const componentNameCapital = (name, glue = '') => {
   if (typeof name !== 'string') {
     return null;
   }
   return name
     .split('-')
     .map(x => x.charAt(0).toUpperCase() + x.slice(1))
-    .join('');
+    .join(glue);
 };
 
 (() => {
-  // create-component component-name
   const componentName = process.argv[2];
   const componentsFolder = `${__dirname}/../src/scss/components`;
 
+  const componentFolder = `${componentsFolder}/c-${componentName}`;
+  const scssFile = `_c-${componentName}.scss`;
+  const twigFile = `c-${componentName}.twig`;
+  const styleguideFile = `c-${componentName}-styleguide.twig`;
+
   console.log(`
-    Component Name: ${componentNameCapital(componentName)}
-    Component Folder: ${componentsFolder}
+    Creating component
+    --------------------------------------------------------
+    Component Name: c-${componentName}
+    Styleguide Section: ${componentNameCapital(componentName)}
   `);
 
-  const componentFolder = `${componentsFolder}/c-${componentName}`;
   if (fs.existsSync(componentFolder)) {
-    console.log('Component already exists');
+    console.error('Component already exists');
   } else {
     shell.mkdir('-p', componentFolder);
     if (!fs.existsSync(componentFolder)) {
-      console.log('Component folder could not created.');
+      console.error('Component folder could not created.');
       return;
     }
-
-    const scssFile = `_c-${componentName}.scss`;
-    const twigFile = `c-${componentName}.twig`;
-    const styleguideFile = `c-${componentName}-styleguide.twig`;
 
     shell.touch(`${componentFolder}/${scssFile}`);
 
     if (!fs.existsSync(`${componentFolder}/${scssFile}`)) {
-      console.log('Component scss file could not created.');
+      console.error('Component scss file could not created.');
       return;
     }
 
     // Scss Template
     const scssTemplate = `//\n// ${componentNameCapital(
       componentName,
+      ' ',
     )}\n//\n// Markup: ${styleguideFile}\n//\n// Styleguide Components.${componentNameCapital(
       componentName,
-    )}\n//\n\n.c-${componentName} {\n  \n}\n`;
+    )}\n//\n\n.c-${componentName} {\n  // Do your stuff\n}\n`;
 
     fs.writeFileSync(`${componentFolder}/${scssFile}`, scssTemplate);
 
@@ -62,7 +79,7 @@ const componentNameCapital = name => {
     shell.touch(`${componentFolder}/${styleguideFile}`);
 
     if (!fs.existsSync(`${componentFolder}/${styleguideFile}`)) {
-      console.log('Component styleguide template file could not created.');
+      console.error('Component styleguide template file could not created.');
       return;
     }
 
