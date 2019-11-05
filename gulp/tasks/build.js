060ad7635298998ef2e82bbe000900fc2efb2ed8
Hasan Aydoğdu <hsnaydd@gmail.com>
2019-11-05 17:04:31 +0300

    Move log and notify gulp tasks into build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,17 +1,25 @@
 import { series, parallel } from 'gulp';
+import c from 'ansi-colors';
 
+import notifier from '../utils/notifier';
 import { clearDist } from './clear';
 import html from './html';
 import scripts from './scripts';
 import styles from './styles';
 import copy from './copy';
-import { notifyBuildComplete } from './notify';
-import { logBuildSuccess } from './log';
 
-export default series(
-  clearDist,
-  parallel(html, scripts, copy),
-  parallel(styles),
-  notifyBuildComplete,
-  logBuildSuccess,
-);
+function completed(cb) {
+  notifier('Build işlemi başarılı bir şekilde tamamlandı.');
+
+  // eslint-disable-next-line no-console
+  console.log(
+    c.green(
+      '\n==============================================\n' +
+        'Build işlemi başarılı bir şekilde tamamlandı.' +
+        '\n==============================================\n',
+    ),
+  );
+  cb();
+}
+
+export default series(clearDist, parallel(html, scripts, copy), parallel(styles), completed);
--- gulp/tasks/notify.js
+++
@@ -1,7 +0,0 @@
-import notifier from '../utils/notifier';
-
-// eslint-disable-next-line import/prefer-default-export
-export function notifyBuildComplete(cb) {
-  notifier('Build işlemi başarılı bir şekilde tamamlandı.');
-  cb();
-}
