ef4b12746c7b2ae403abd23deda365443d1c7cff
Hasan Aydoğdu <hsnaydd@gmail.com>
2019-11-11 12:25:35 +0300

    Remove styleguide build npm script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@
 import build from './gulp/tasks/build';
 import start from './gulp/tasks/start';
 
-import styleguideStart from './gulp/tasks/styleguide/start';
+import styleguide from './gulp/tasks/styleguide/start';
 import styleguideBuild from './gulp/tasks/styleguide/build';
 
-export { build, start, styleguideBuild, styleguideStart };
+export { build, start, styleguideBuild, styleguide };
