1dc3e7cbd91ba091aa692f9a75d465f56158a001
Hasan Aydoğdu <hsnaydd@gmail.com>
2019-11-12 11:44:37 +0300

    Add `dotenv` ability

--- gulp/utils/env.js
+++ gulp/utils/env.js
@@ -1,10 +1,30 @@
-import minimist from 'minimist';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
 import configs from '../../configs';
 
-const argv = minimist(process.argv.slice(2));
+const { NODE_ENV } = process.env;
 
-export const isProduction = argv.env === 'production';
+if (!NODE_ENV) {
+  throw new Error('The NODE_ENV environment variable is required but was not specified.');
+}
+
+const dotenvFiles = [
+  `.env.${NODE_ENV}.local`,
+  `.env.${NODE_ENV}`,
+  NODE_ENV !== 'test' && '.env.local',
+  '.env',
+].filter(Boolean);
+
+dotenvFiles.forEach(dotenvFile => {
+  if (fs.existsSync(dotenvFile)) {
+    dotenvExpand(dotenv.config({ path: dotenvFile }));
+  }
+});
+
+export const isProduction = NODE_ENV === 'production';
 
 export const envPath = isProduction ? configs.paths.dist : configs.paths.dev;
 
-export default argv.env;
+export default NODE_ENV;
