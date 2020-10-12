73b6bbd4fc694d0b4c010830725ed3381a407e99
yoktav <iletisimyilmazoktav@gmail.com>
2020-10-12 16:34:38 +0300

    Replace extractors to defaultExtractor in PurgeCSS options

--- config/purgecss.config.js
+++ config/purgecss.config.js
@@ -5,12 +5,6 @@ export default {
   keyframes: true,
   variables: true,
   whitelistPatterns: [/is-[\S]*/g, /has-[\S]*/g],
-  extractors: [
-    {
-      extractor: (content) => {
-        return content.match(/[a-z-_\\@%0-9]+/g) || []; // Compare class names including '@', '%' and '\'
-      },
-      extensions: ['css', 'html'],
-    },
-  ],
+  // Compare class names including '@', '%' and '\'
+  defaultExtractor: (content) => content.match(/[a-z-_\\@%0-9]+/g) || [],
 };
