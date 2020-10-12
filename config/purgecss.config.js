80c95bbdddaa580506b37656c605d6872202f08b
yoktav <iletisimyilmazoktav@gmail.com>
2020-10-12 15:49:50 +0300

    Add extractors option to PurgeCSS

--- config/purgecss.config.js
+++ config/purgecss.config.js
@@ -4,4 +4,12 @@ export default {
   content: [`${paths.dist}/*.html`],
   keyframes: true,
   variables: true,
+  extractors: [
+    {
+      extractor: (content) => {
+        return content.match(/[a-z-_\\@%0-9]+/g) || []; // Compare class names including '@', '%' and '\'
+      },
+      extensions: ['css', 'html'],
+    },
+  ],
 };
